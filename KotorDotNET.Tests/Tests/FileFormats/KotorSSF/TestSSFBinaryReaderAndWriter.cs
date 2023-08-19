9887700080170a7d397566f20c2109ca2fe5ce08
Nick Hugi <hugi.nicholas@hotmail.com>
2023-08-19 22:26:54 +0800

    Add TalkTable class for accessing latest TLK data

--- KotorDotNET.Tests/Tests/FileFormats/KotorSSF/TestSSFBinaryReaderAndWriter.cs
+++ KotorDotNET.Tests/Tests/FileFormats/KotorSSF/TestSSFBinaryReaderAndWriter.cs
@@ -1,14 +1,14 @@
 ﻿using KotorDotNET.Common.Conversation;
 using KotorDotNET.Common.Creature;
 using KotorDotNET.Common.Data;
-using KotorDotNET.FileFormats.KotorTLK;
+using KotorDotNET.FileFormats.KotorSSF;
 using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Numerics;
 using System.Text;
 
-namespace KotorDotNET.Tests.Tests.FileFormats.KotorTLK
+namespace KotorDotNET.Tests.Tests.FileFormats.KotorSSF
 {
     [TestClass]
     public class TestSSFBinaryReaderAndWriter
@@ -16,45 +16,53 @@ namespace KotorDotNET.Tests.Tests.FileFormats.KotorTLK
         [TestMethod]
         public void ReadAndWrite()
         {
-            var data = File.ReadAllBytes("./Files/test.tlk");
-            var gff = AssertRead(data);
+            var data = File.ReadAllBytes("./Files/test.ssf");
+            var ssf = AssertRead(data);
 
             // Now that it has been confirmed that files are read correctly, the
             // file data will be written then that will be read to confirm that
             // the write was done correctly.
             var stream = new MemoryStream();
-            var writer = new TLKBinaryWriter(stream);
-            writer.Write(gff);
+            var writer = new SSFBinaryWriter(stream);
+            writer.Write(ssf);
             AssertRead(stream.ToArray());
         }
 
-        public TLK AssertRead(byte[] data)
+        public SSF AssertRead(byte[] data)
         {
-            //   | Text          | Sound    |
-            // --|---------------------------
-            // 0 | abcdef        | resref01 |
-            // 1 | ghijklmnop    | resref02 |
-            // 2 | qrstuvwxyz    |          |
-            // --|---------------|----------|
+            var reader = new SSFBinaryReader(data);
+            var ssf = reader.Read();
 
-            var reader = new TLKBinaryReader(data);
-            var tlk = reader.Read();
+            Assert.AreEqual(123075, ssf.Get((CreatureSound)0));
+            Assert.AreEqual(123074, ssf.Get((CreatureSound)1));
+            Assert.AreEqual(123073, ssf.Get((CreatureSound)2));
+            Assert.AreEqual(123072, ssf.Get((CreatureSound)3));
+            Assert.AreEqual(123071, ssf.Get((CreatureSound)4));
+            Assert.AreEqual(123070, ssf.Get((CreatureSound)5));
+            Assert.AreEqual(123069, ssf.Get((CreatureSound)6));
+            Assert.AreEqual(123068, ssf.Get((CreatureSound)7));
+            Assert.AreEqual(123067, ssf.Get((CreatureSound)8));
+            Assert.AreEqual(123066, ssf.Get((CreatureSound)9));
+            Assert.AreEqual(123065, ssf.Get((CreatureSound)10));
+            Assert.AreEqual(123064, ssf.Get((CreatureSound)11));
+            Assert.AreEqual(123063, ssf.Get((CreatureSound)12));
+            Assert.AreEqual(123062, ssf.Get((CreatureSound)13));
+            Assert.AreEqual(123061, ssf.Get((CreatureSound)14));
+            Assert.AreEqual(123060, ssf.Get((CreatureSound)15));
+            Assert.AreEqual(123059, ssf.Get((CreatureSound)16));
+            Assert.AreEqual(123058, ssf.Get((CreatureSound)17));
+            Assert.AreEqual(123057, ssf.Get((CreatureSound)18));
+            Assert.AreEqual(123056, ssf.Get((CreatureSound)19));
+            Assert.AreEqual(123055, ssf.Get((CreatureSound)20));
+            Assert.AreEqual(123054, ssf.Get((CreatureSound)21));
+            Assert.AreEqual(123053, ssf.Get((CreatureSound)22));
+            Assert.AreEqual(123052, ssf.Get((CreatureSound)23));
+            Assert.AreEqual(123051, ssf.Get((CreatureSound)24));
+            Assert.AreEqual(123050, ssf.Get((CreatureSound)25));
+            Assert.AreEqual(123049, ssf.Get((CreatureSound)26));
+            Assert.AreEqual(123048, ssf.Get((CreatureSound)27));
 
-            // Check the language is correct
-            Assert.AreEqual(Language.ENGLISH, tlk.Language);
-            // Check there are the correct number of entries
-            Assert.AreEqual(3, tlk.Entries.Count);
-            // Check the first entry is correct
-            Assert.AreEqual("abcdef",   tlk.Get(0).Text);
-            Assert.AreEqual("resref01", tlk.Get(0).ResRef.Get());
-            // Check the second entry is correct
-            Assert.AreEqual("ghijklmnop", tlk.Get(1).Text);
-            Assert.AreEqual("resref02",   tlk.Get(1).ResRef.Get());
-            // Check the third entry is correct
-            Assert.AreEqual("qrstuvwxyz", tlk.Get(2).Text);
-            Assert.AreEqual("",           tlk.Get(2).ResRef.Get());
-
-            return tlk;
+            return ssf;
         }
     }
 }
--- KotorDotNET/FileFormats/KotorTLK/TLK.cs
+++ KotorDotNET/FileFormats/KotorTLK/TLK.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using KotorDotNET.Common;
 using KotorDotNET.Common.Conversation;
 using KotorDotNET.Common.Data;
 
@@ -11,7 +12,7 @@ namespace KotorDotNET.FileFormats.KotorTLK
     public class TLK
     {
         public Language Language { get; set; }
-        public List<TLKEntry> Entries { get; set; }
+        public List<TalkTableEntry> Entries { get; set; }
 
         public TLK()
         {
@@ -26,34 +27,16 @@ namespace KotorDotNET.FileFormats.KotorTLK
 
         public void Add(string text)
         {
-            Entries.Add(new TLKEntry(text));
+            Entries.Add(new TalkTableEntry(text));
         }
         public void Add(string text, ResRef resref)
         {
-            Entries.Add(new TLKEntry(text, resref));
+            Entries.Add(new TalkTableEntry(text, resref));
         }
 
-        public TLKEntry Get(int index)
+        public TalkTableEntry Get(int index)
         {
             return Entries[index];
-        }
-    }
-
-    public class TLKEntry
-    {
-        public string Text { get; set; }
-        public ResRef ResRef { get; set; }
-
-        public TLKEntry(string text, ResRef resRef)
-        {
-            Text = text;
-            ResRef = resRef;
-        }
-
-        public TLKEntry(string text)
-        {
-            Text = text;
-            ResRef = new();
         }
     }
 }
