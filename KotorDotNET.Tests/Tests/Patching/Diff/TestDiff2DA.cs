02615eca767f36491ddb55ec30e4e4d88fb96bdf
Nick Hugi <hugi.nicholas@hotmail.com>
2023-08-20 21:54:52 +0800

    Diff2DA class for comparing 2DAs

---
+++ KotorDotNET.Tests/Tests/Patching/Diff/TestDiff2DA.cs
@@ -0,0 +1,98 @@
+﻿// Licensed to the .NET Foundation under one or more agreements.
+// The .NET Foundation licenses this file to you under the MIT license.
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using KotorDotNET.FileFormats.Kotor2DA;
+using KotorDotNET.Patching.Diff;
+
+namespace KotorDotNET.Tests.Tests.Patching.Diff
+{
+    [TestClass]
+    public class TestDiff2DA
+    {
+        [TestMethod]
+        public void IsSame_ColumnCountMismatch()
+        {
+            var older = new TwoDA();
+            older.AddColumn("ABC");
+            older.AddColumn("123");
+
+            var newer = new TwoDA();
+            newer.AddColumn("ABC");
+
+            var diff = new Diff2DA(older, newer);
+
+            // Columns do not match so the 2DAs are not the same.
+            Assert.IsFalse(diff.IsSame());
+        }
+
+        [TestMethod]
+        public void IsSame_RowCountMismatch()
+        {
+            var older = new TwoDA();
+            older.AddColumn("A");
+            older.AddColumn("B");
+            older.AddRow("1");
+            older.AddRow("2");
+
+            var newer = new TwoDA();
+            newer.AddColumn("A");
+            newer.AddColumn("B");
+            newer.AddRow("1");
+
+            var diff = new Diff2DA(older, newer);
+
+            // One has more rows then the others so the 2DAs are not the same.
+            Assert.IsFalse(diff.IsSame());
+        }
+
+        [TestMethod]
+        public void IsSame_CellMismatch()
+        {
+            var older = new TwoDA();
+            older.AddColumn("A");
+            older.AddColumn("B");
+            older.AddRow("1");
+            older.AddRow("2");
+
+            var newer = new TwoDA();
+            newer.AddColumn("A");
+            newer.AddColumn("B");
+            newer.AddRow("1");
+            newer.AddRow("2");
+            newer.Row(0).SetCell("A", "asdf");
+
+            var diff = new Diff2DA(older, newer);
+
+            // One has more rows then the others so the 2DAs are not the same.
+            Assert.IsFalse(diff.IsSame());
+        }
+
+        [TestMethod]
+        public void IsSame_AreMatching()
+        {
+            var older = new TwoDA();
+            older.AddColumn("A");
+            older.AddColumn("B");
+            older.AddRow("1");
+            older.AddRow("2");
+            older.Row(0).SetCell("A", "asdf");
+
+            var newer = new TwoDA();
+            newer.AddColumn("A");
+            newer.AddColumn("B");
+            newer.AddRow("1");
+            newer.AddRow("2");
+            newer.Row(0).SetCell("A", "asdf");
+
+            var diff = new Diff2DA(older, newer);
+
+            // One has more rows then the others so the 2DAs are not the same.
+            Assert.IsTrue(diff.IsSame());
+        }
+    }
+}
---
+++ KotorDotNET/Patching/Diff/Diff2DA.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using KotorDotNET.FileFormats.Kotor2DA;
+
+namespace KotorDotNET.Patching.Diff
+{
+    public class Diff2DA
+    {
+        public TwoDA Older { get; }
+        public TwoDA Newer { get; }
+
+        public Diff2DA(TwoDA older, TwoDA newer)
+        {
+            Older = older;
+            Newer = newer;
+        }
+
+        public List<IModifier<TwoDA>> Find()
+        {
+            var modifiers = new List<IModifier<TwoDA>>();
+
+            // TODO
+
+            return modifiers;
+        }
+
+        public bool IsSame()
+        {
+            var oldColumnHeaders = new HashSet<string>(Older.ColumnHeaders());
+            var newColumnHeaders = new HashSet<string>(Newer.ColumnHeaders());
+
+            if (!oldColumnHeaders.SetEquals(newColumnHeaders))
+            {
+                return false;
+            }
+
+            if (Older.Rows().Count != Newer.Rows().Count)
+            {
+                return false;
+            }
+
+            foreach (var oldRow in Older.Rows())
+            {
+                var newRow = Newer.Row(oldRow.Index);
+
+                foreach (var columnHeader in Older.ColumnHeaders())
+                {
+                    var oldValue = oldRow.GetCell(columnHeader);
+                    var newValue = newRow.GetCell(columnHeader);
+
+                    if (oldValue != newValue)
+                    {
+                        return false;
+                    }
+                }
+            }
+
+            return true;
+        }
+    }
+}
