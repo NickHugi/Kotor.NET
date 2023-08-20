dd0d1608bca096ede76070bca611e86fdd71a982
Nick Hugi <hugi.nicholas@hotmail.com>
2023-08-20 20:49:32 +0800

    Add KotorPath for storing case-sensitive paths

---
+++ KotorDotNET/Common/Data/KotorPath.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KotorDotNET.Common.Data
+{
+    /// <summary>
+    /// A special class designed to store a path that accounts for the case-sensitivity
+    /// on unix-like systems.
+    /// </summary>
+    public class KotorPath
+    {
+        /// <summary>
+        /// The stored path.
+        /// </summary>
+        public string Value { get; private set; }
+
+        public KotorPath(string path)
+        {
+            // TODO - check casing on unix systems
+            Value = path;
+        }
+
+        /// <summary>
+        /// Returns a new KotorPath instance with the specificied path adjoined to
+        /// the end of the current instance.
+        /// </summary>
+        /// <param name="path">The extra path to add to the end.</param>
+        /// <returns>A new KotorPath instance with the old and new paths joined.</returns>
+        public KotorPath Join(string path)
+        {
+            return Path.Join(Value, path);
+        }
+
+        public static implicit operator KotorPath(string path)
+        {
+            return new KotorPath(path);
+        }
+
+        public static implicit operator string(KotorPath path)
+        {
+            return path.Value;
+        }
+    }
+}
--- KotorDotNET/Common/Installation.cs
+++ KotorDotNET/Common/Installation.cs
@@ -1,4 +1,5 @@
-﻿using KotorDotNET.ResourceContainers;
+﻿using KotorDotNET.Common.Data;
+using KotorDotNET.ResourceContainers;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -13,67 +14,67 @@ namespace KotorDotNET.Common
     /// </summary>
     public class Installation
     {
-        public string GamePath { get; private set; }
+        public KotorPath GamePath { get; private set; }
         public Chitin Chitin { get; private set; }
-        public string ChitinPath { get; private set; }
+        public KotorPath ChitinPath { get; private set; }
         public TalkTable TalkTable { get; private set; }
-        public string TalkTablePath { get; private set; }
+        public KotorPath TalkTablePath { get; private set; }
         public CapsuleFolder Modules { get; private set; }
-        public string ModulesPath { get; private set; }
+        public KotorPath ModulesPath { get; private set; }
         public CapsuleFolder Rims { get; private set; }
-        public string RimsPath { get; private set; }
+        public KotorPath RimsPath { get; private set; }
         public CapsuleFolder Lips { get; private set; }
-        public string LipsPath { get; private set; }
+        public KotorPath LipsPath { get; private set; }
         public Capsule TexturesHigh { get; private set; }
         public Capsule TexturesMed { get; private set; }
         public Capsule TexturesLow { get; private set; }
         public Capsule TexturesGUI { get; private set; }
-        public string TexturePacksPath { get; private set; }
+        public KotorPath TexturePacksPath { get; private set; }
         public ResourceFolder Override { get; private set; }
-        public string OverridePath { get; private set; }
+        public KotorPath OverridePath { get; private set; }
         public ResourceFolder Music { get; private set; }
-        public string MusicPath { get; private set; }
+        public KotorPath MusicPath { get; private set; }
         public ResourceFolder Sounds { get; private set; }
-        public string SoundsPath { get; private set; }
+        public KotorPath SoundsPath { get; private set; }
         public ResourceFolder Voices { get; private set; }
-        public string VoicesPath { get; private set; }
+        public KotorPath VoicesPath { get; private set; }
 
         public Installation(string gameDirectory)
         {
             // TODO - path handling
             GamePath = gameDirectory;
 
-            ChitinPath = gameDirectory + "/chitin.key";
+            ChitinPath = GamePath.Join("chitin.key");
             Chitin = new Chitin(gameDirectory);
 
-            TalkTablePath = gameDirectory + "/dialog.tlk";
+            TalkTablePath = GamePath.Join("dialog.tlk");
             TalkTable = new TalkTable(TalkTablePath);
 
-            ModulesPath = gameDirectory + "/modules";
+            ModulesPath = GamePath.Join("modules");
             Modules = new CapsuleFolder(ModulesPath);
 
-            RimsPath = gameDirectory + "/rims";
+            RimsPath = GamePath.Join("rims");
             Rims = new CapsuleFolder(RimsPath);
 
-            LipsPath = gameDirectory + "lips";
+            LipsPath = GamePath.Join("lips");
             Lips = new CapsuleFolder(LipsPath);
 
             TexturePacksPath = gameDirectory + "TexturePacks/";
-            TexturesLow = new Capsule(TexturePacksPath + "/swpc_tex_tpc.erf");
-            TexturesMed = new Capsule(TexturePacksPath + "/swpc_tex_tpb.erf");
-            TexturesHigh = new Capsule(TexturePacksPath + "/swpc_tex_tpa.erf");
-            TexturesGUI = new Capsule(TexturePacksPath + "/swpc_tex_gui.erf");
+            TexturesLow = new Capsule(TexturePacksPath.Join("/swpc_tex_tpc.erf"));
+            TexturesMed = new Capsule(TexturePacksPath.Join("/swpc_tex_tpb.erf"));
+            TexturesHigh = new Capsule(TexturePacksPath.Join("/swpc_tex_tpa.erf"));
+            TexturesGUI = new Capsule(TexturePacksPath.Join("/swpc_tex_gui.erf"));
 
-            OverridePath = gameDirectory + "/override";
+            OverridePath = GamePath.Join("override");
             Override = new ResourceFolder(OverridePath);
 
-            MusicPath = gameDirectory + "/streammusic";
+            MusicPath = GamePath.Join("streammusic");
             Music = new ResourceFolder(MusicPath);
 
-            SoundsPath = gameDirectory + "/streamsounds";
+            SoundsPath = GamePath.Join("streamsounds");
             Sounds = new ResourceFolder(SoundsPath);
 
-            VoicesPath = gameDirectory + "/streamwaves";
+            VoicesPath = GamePath.Join("streamwaves");
             Voices = new ResourceFolder(VoicesPath);
         }
     }
