c6a76052bdb905fc6055f0731998d6e08fa4a2a4
Nick Hugi <hugi.nicholas@hotmail.com>
2023-09-16 00:10:16 +0800

    Basic compiler Lexer and Yacc setup using Calculator template

---
+++ KotorDotNET.Compiler/Calculator/Calculator.Scanner.cs
@@ -0,0 +1,23 @@
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace KotorDotNET_Compiler.Calculator
+{
+    internal partial class CalculatorScanner
+    {
+
+        void GetNumber()
+        {
+            yylval.s = yytext;
+            yylval.n = int.Parse(yytext);
+        }
+
+		public override void yyerror(string format, params object[] args)
+		{
+			base.yyerror(format, args);
+			Console.WriteLine(format, args);
+			Console.WriteLine();
+		}
+    }
+}
