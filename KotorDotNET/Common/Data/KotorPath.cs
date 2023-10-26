394d1c54ce6a964441d0edf2353a804a015e6537
Benjamin Auquite <halomastar@gmail.com>
2023-10-26 18:50:06 -0500

    implement KotorPath

--- KotorDotNET/Common/Data/KotorPath.cs
+++ KotorDotNET/Common/Data/KotorPath.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Runtime.InteropServices;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -15,33 +16,85 @@ namespace KotorDotNET.Common.Data
         /// <summary>
         /// The stored path.
         /// </summary>
-        public string Value { get; private set; }
+        public string Value { get; }
 
         public KotorPath(string path)
         {
-            // TODO - check casing on unix systems
             Value = path;
         }
 
         /// <summary>
-        /// Returns a new KotorPath instance with the specificied path adjoined to
+        /// Returns a new KotorPath instance with the specified path adjoined to
         /// the end of the current instance.
         /// </summary>
         /// <param name="path">The extra path to add to the end.</param>
         /// <returns>A new KotorPath instance with the old and new paths joined.</returns>
-        public KotorPath Join(string path)
+        public KotorPath Join(string path) => new(Path.Join(Value, path));
+
+        [DllImport("libc", SetLastError = true)]
+        private static extern IntPtr opendir(string name);
+
+        [DllImport("libc", SetLastError = true)]
+        private static extern IntPtr readdir(IntPtr dirp);
+
+        [DllImport("libc", SetLastError = true)]
+        private static extern int closedir(IntPtr dirp);
+
+        [StructLayout(LayoutKind.Sequential)]
+        private struct Dirent
         {
-            return Path.Join(Value, path);
+            public IntPtr d_ino;
+            public IntPtr d_off;
+            public ushort d_reclen;
+            public byte d_type;
+            [MarshalAs(UnmanagedType.ByValTStr, SizeConst = 256)]
+            public string d_name;
         }
 
-        public static implicit operator KotorPath(string path)
+        private bool ShouldResolveCase() => !OperatingSystem.IsWindows() && Path.IsPathRooted(Value);
+
+        private string NormalizePath()
         {
-            return new KotorPath(path);
+            if (!ShouldResolveCase())
+                return Value;
+
+            StringBuilder currentPath = new("/");
+            foreach (string segment in Value.Split('/'))
+            {
+                string? resolvedSegment = ResolveCase(currentPath.ToString(), segment);
+                if (string.IsNullOrEmpty(resolvedSegment))
+                    return Value; // Path not resolved, return original value
+
+                currentPath.Append(resolvedSegment);
+                currentPath.Append('/');
+            }
+
+            return currentPath.ToString().TrimEnd('/');
         }
 
-        public static implicit operator string(KotorPath path)
+        private string? ResolveCase(string currentPath, string segment)
         {
-            return path.Value;
+            IntPtr dirPtr = opendir(currentPath);
+            if (dirPtr == IntPtr.Zero)
+                return null; // Cannot open directory
+
+            IntPtr direntPtr;
+            while ((direntPtr = readdir(dirPtr)) != IntPtr.Zero)
+            {
+                Dirent dirent = Marshal.PtrToStructure<Dirent>(direntPtr);
+                if (segment.Equals(dirent.d_name, StringComparison.OrdinalIgnoreCase))
+                {
+                    closedir(dirPtr);
+                    return dirent.d_name;
+                }
+            }
+
+            closedir(dirPtr);
+            return null; // Segment not found
         }
+
+        public static implicit operator KotorPath(string strPath) => new(strPath);
+        public static implicit operator string(KotorPath kPath) => kPath.NormalizePath();
     }
+
 }
