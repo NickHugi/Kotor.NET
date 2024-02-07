969631058fb8ac196ebb777e8dbda36a0f29a6c3
Nick Hugi <hugi.nicholas@hotmail.com>
2024-02-07 22:03:51 +0800

    move patcher parser to parser project

--- Kotor.NET.Patcher/Exceptions/PatchingParserException.cs
+++ Kotor.NET.Patcher/Exceptions/PatchingParserException.cs
@@ -4,7 +4,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace Kotor.NET.Exceptions
+namespace Kotor.NET.Patcher.Exceptions
 {
     public class PatchingParserException : Exception
     {
