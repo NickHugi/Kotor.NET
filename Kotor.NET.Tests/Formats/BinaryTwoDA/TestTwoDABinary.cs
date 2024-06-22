8dca63d987683ef4d1ac0d9a7e37a6e4216f74ba
Nick Hugi <hugi.nicholas@hotmail.com>
2024-06-22 20:24:28 +0800

    Add support for reading/writing binary the 2DA format

---
+++ Kotor.NET.Tests/Formats/BinaryTwoDA/TestTwoDABinary.cs
@@ -0,0 +1,54 @@
+using System.Reflection.PortableExecutable;
+using Kotor.NET.Common.Data;
+using Kotor.NET.Extensions;
+using Kotor.NET.Formats.Binary2DA;
+using Microsoft.VisualStudio.TestPlatform.CommunicationUtilities.Resources;
+
+namespace Kotor.NET.Tests.Formats.BinaryTwoDA;
+
+public class TestTwoDABinary
+{
+    public static readonly string File1Filepath = "Formats/BinaryTwoDA/file1.2da";
+
+    [SetUp]
+    public void Setup()
+    {
+        
+    }
+
+    private TwoDABinary GetBinaryTwoDA(byte[] data)
+    {
+        var reader = new BinaryReader(new MemoryStream(data));
+        return new TwoDABinary(reader);
+    }
+   
+    [Test]
+    public void Test_ReadFile1()
+    {
+        var binaryTwoDA = GetBinaryTwoDA(File.ReadAllBytes(File1Filepath));
+
+        Assert.That(binaryTwoDA.FileHeader.FileType, Is.EqualTo("2DA "));
+        Assert.That(binaryTwoDA.FileHeader.FileVersion, Is.EqualTo("V2.b"));
+
+        Assert.That(binaryTwoDA.ColumnHeaders.Count(), Is.EqualTo(3));
+        Assert.That(binaryTwoDA.CellValues.Count(), Is.EqualTo(9));
+
+        Assert.That(binaryTwoDA.ColumnHeaders[0], Is.EqualTo("col3"));
+        Assert.That(binaryTwoDA.ColumnHeaders[1], Is.EqualTo("col2"));
+        Assert.That(binaryTwoDA.ColumnHeaders[2], Is.EqualTo("col1"));
+
+        Assert.That(binaryTwoDA.RowHeaders[0], Is.EqualTo("10"));
+        Assert.That(binaryTwoDA.RowHeaders[1], Is.EqualTo("1"));
+        Assert.That(binaryTwoDA.RowHeaders[2], Is.EqualTo("2"));
+
+        Assert.That(binaryTwoDA.CellValues[0], Is.EqualTo("ghi"));
+        Assert.That(binaryTwoDA.CellValues[1], Is.EqualTo("def"));
+        Assert.That(binaryTwoDA.CellValues[2], Is.EqualTo("abc"));
+        Assert.That(binaryTwoDA.CellValues[3], Is.EqualTo("123"));
+        Assert.That(binaryTwoDA.CellValues[4], Is.EqualTo("ghi"));
+        Assert.That(binaryTwoDA.CellValues[5], Is.EqualTo("def"));
+        Assert.That(binaryTwoDA.CellValues[6], Is.EqualTo("abc"));
+        Assert.That(binaryTwoDA.CellValues[7], Is.EqualTo(""));
+        Assert.That(binaryTwoDA.CellValues[8], Is.EqualTo("123"));
+    }
+}
---
+++ Kotor.NET/Formats/Binary2DA/TwoDABinaryFileHeader.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Kotor.NET.Extensions;
+
+namespace Kotor.NET.Formats.Binary2DA;
+
+public class TwoDABinaryFileHeader
+{
+    public static readonly int SIZE = 11;
+    public static readonly IReadOnlyList<string> FILE_TYPES = new List<string>()
+    {
+        "2DA ",
+    };
+    public static readonly string FILE_VERSION = "V2.b";
+
+    public string FileType { get; set; } = "2DA ";
+    public string FileVersion { get; set; } = "v2.b";
+
+    public TwoDABinaryFileHeader()
+    {
+
+    }
+
+    public TwoDABinaryFileHeader(BinaryReader reader)
+    {
+        FileType = reader.ReadString(4);
+        FileVersion = reader.ReadString(4);
+        var linebreak = reader.ReadString(1);
+    }
+
+    public void Write(BinaryWriter writer)
+    {
+        writer.Write(FileType.PadRight(4).Truncate(4), 0);
+        writer.Write(FileVersion.PadRight(4).Truncate(4), 0);
+        writer.Write("\n", 0);
+    }
+}
