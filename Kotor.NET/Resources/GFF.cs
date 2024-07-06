ed770ed7d5752ea5e62b7d788ce433b39d20b8f9
Nick Hugi <hugi.nicholas@hotmail.com>
2024-07-06 20:47:34 +0800

    1

--- Kotor.NET/Resources/GFF.cs
+++ Kotor.NET/Resources/GFF.cs
@@ -1,12 +1,105 @@
-﻿using System;
+﻿using Kotor.NET.Common.Data;
+using System;
+using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace Kotor.NET.Resources
+namespace Kotor.NET.Resources;
+
+public class GFF
 {
-    internal class GFF
+    public GFFStruct Root { get; set; }
+}
+
+public class GFFStruct
+{
+    public byte? GetUInt8(string fieldName)
     {
+        throw new NotImplementedException();
+    }
+    public sbyte? GetInt8(string fieldName)
+    {
+        throw new NotImplementedException();
+    }
+    public ushort? GetUInt16(string fieldName)
+    {
+        throw new NotImplementedException();
+    }
+    public short? GetInt16(string fieldName)
+    {
+        throw new NotImplementedException();
+    }
+    public uint? GetUInt32(string fieldName)
+    {
+        throw new NotImplementedException();
+    }
+    public int? GetInt32(string fieldName)
+    {
+        throw new NotImplementedException();
+    }
+    public ulong? GetUInt64(string fieldName)
+    {
+        throw new NotImplementedException();
+    }
+    public long? GetInt64(string fieldName)
+    {
+        throw new NotImplementedException();
+    }
+    public float? GetSingle(string fieldName)
+    {
+        throw new NotImplementedException();
+    }
+    public double? GetDouble(string fieldName)
+    {
+        throw new NotImplementedException();
+    }
+    public string? GetString(string fieldName)
+    {
+        throw new NotImplementedException();
+    }
+    public ResRef? GetResRef(string fieldName)
+    {
+        throw new NotImplementedException();
+    }
+    public LocalisedString? GetLocalizedString(string fieldName)
+    {
+        throw new NotImplementedException();
+    }
+    public byte[]? GetBinary(string fieldName)
+    {
+        throw new NotImplementedException();
+    }
+    public GFFStruct GetStruct(string fieldName)
+    {
+        throw new NotImplementedException();
+    }
+    public GFFList GetList(string fieldName)
+    {
+        throw new NotImplementedException();
+    }
+    public Vector3 GetVector3(string fieldName)
+    {
+        throw new NotImplementedException();
+    }
+    public Vector4 GetVector4(string fieldName)
+    {
+        throw new NotImplementedException();
+    }
+}
+
+public class GFFList
+{
+    private List<GFFStruct> Structs = new();
+
+    public int Count()
+    {
+        return Structs.Count;
+    }
+
+    public GFFStruct StructAt(int index)
+    {
+        return Structs[index];
     }
 }
