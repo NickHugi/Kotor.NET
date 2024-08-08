bf86203da36cf3a6beaebb95b0352fe8fcdea27a
Nick Hugi <hugi.nicholas@hotmail.com>
2024-08-08 21:28:49 +0800

    Add IFO class

---
+++ Kotor.NET.Tests/Resources/KotorIFO/TestIFO.cs
@@ -0,0 +1,117 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Kotor.NET.Common;
+using Kotor.NET.Resources.KotorIFO;
+
+namespace Kotor.NET.Tests.Resources.KotorIFO;
+
+public class TestIFO
+{
+    public static readonly string File1Filepath = "Resources/KotorIFO/file1.ifo";
+
+    [Fact]
+    public void Getters()
+    {
+        // Setup
+        var ifo = IFO.FromFile(File1Filepath);
+
+        // Assert
+        Assert.Equal("m02aa", ifo.VoiceOverID);
+        Assert.Equal("tar_m02aa", ifo.Name.GetSubstring(Language.English, Gender.MaleOrNeutral));
+        Assert.Equal("", ifo.Tag);
+        Assert.Equal("m02aa", ifo.EntryArea);
+        Assert.Equal(3, ifo.ModEntryX);
+        Assert.Equal(4, ifo.ModEntryY);
+        Assert.Equal(5, ifo.ModEntryZ);
+        Assert.Equal(1, ifo.ModEntryDirectionX);
+        Assert.Equal(2, ifo.ModEntryDirectionY);
+        Assert.Equal(10, ifo.XPScale);
+        Assert.Equal("", ifo.OnHeartbeat);
+        Assert.Equal("", ifo.OnModLoad);
+        Assert.Equal("", ifo.OnModStart);
+        Assert.Equal("", ifo.OnClientEnter);
+        Assert.Equal("", ifo.OnClientLeave);
+        Assert.Equal("", ifo.OnActivateItem);
+        Assert.Equal("", ifo.OnAcquireItem);
+        Assert.Equal("", ifo.OnUserDefined);
+        Assert.Equal("", ifo.OnUnAcquireItem);
+        Assert.Equal("nw_o0_death", ifo.OnPlayerDeath);
+        Assert.Equal("nw_o0_dying", ifo.OnPlayerDying);
+        Assert.Equal("", ifo.OnPlayerLevelUp);
+        Assert.Equal("nw_o0_respawn", ifo.OnSpawnButtonDown);
+
+        Assert.Single(ifo.ModAreaList);
+        var area0 = ifo.ModAreaList[0];
+        Assert.Equal("m02aa", area0.ResRef);
+    }
+
+    [Fact]
+    public void Setters()
+    {
+        // Setup
+        var ifo = IFO.FromFile(File1Filepath);
+        ifo.VoiceOverID = "voice_over_id";
+        ifo.Name.StringRef = 12345;
+        ifo.Tag = "module_tag";
+        ifo.EntryArea = "entry_area";
+        ifo.ModEntryX = 400.0f;
+        ifo.ModEntryY = 500.0f;
+        ifo.ModEntryZ = 600.0f;
+        ifo.ModEntryDirectionX = 2.0f;
+        ifo.ModEntryDirectionY = 3.0f;
+        ifo.XPScale = 10;
+        ifo.OnHeartbeat = "heartbeat";
+        ifo.OnModLoad = "modload";
+        ifo.OnModStart = "modstart";
+        ifo.OnClientEnter = "cliententer";
+        ifo.OnClientLeave = "clientleave";
+        ifo.OnActivateItem = "activateitem";
+        ifo.OnAcquireItem = "acquireitem";
+        ifo.OnUserDefined = "userdefined";
+        ifo.OnUnAcquireItem = "unacquireitem";
+        ifo.OnPlayerDeath = "playerdeath";
+        ifo.OnPlayerDying = "playerdying";
+        ifo.OnPlayerLevelUp = "playerlevelup";
+        ifo.OnSpawnButtonDown = "spawnbutton";
+
+        ifo.ModAreaList.Clear();
+        ifo.ModAreaList.Add("area1");
+        ifo.ModAreaList.Add("area2");
+
+        // Assert
+        Assert.Equal("voice_over_id", ifo.VoiceOverID);
+        Assert.Equal(12345, ifo.Name.StringRef);
+        Assert.Equal("module_tag", ifo.Tag);
+        Assert.Equal("entry_area", ifo.EntryArea);
+        Assert.Equal(400.0f, ifo.ModEntryX);
+        Assert.Equal(500.0f, ifo.ModEntryY);
+        Assert.Equal(600.0f, ifo.ModEntryZ);
+        Assert.Equal(2.0f, ifo.ModEntryDirectionX);
+        Assert.Equal(3.0f, ifo.ModEntryDirectionY);
+        Assert.Equal(10, ifo.XPScale);
+        Assert.Equal("heartbeat", ifo.OnHeartbeat);
+        Assert.Equal("modload", ifo.OnModLoad);
+        Assert.Equal("modstart", ifo.OnModStart);
+        Assert.Equal("cliententer", ifo.OnClientEnter);
+        Assert.Equal("clientleave", ifo.OnClientLeave);
+        Assert.Equal("activateitem", ifo.OnActivateItem);
+        Assert.Equal("acquireitem", ifo.OnAcquireItem);
+        Assert.Equal("userdefined", ifo.OnUserDefined);
+        Assert.Equal("unacquireitem", ifo.OnUnAcquireItem);
+        Assert.Equal("playerdeath", ifo.OnPlayerDeath);
+        Assert.Equal("playerdying", ifo.OnPlayerDying);
+        Assert.Equal("playerlevelup", ifo.OnPlayerLevelUp);
+        Assert.Equal("spawnbutton", ifo.OnSpawnButtonDown);
+
+        var area0 = ifo.ModAreaList.ElementAtOrDefault(0);
+        Assert.NotNull(area0);
+        Assert.Equal("area1", area0.ResRef);
+
+        var area1 = ifo.ModAreaList.ElementAtOrDefault(1);
+        Assert.NotNull(area1);
+        Assert.Equal("area2", area1.ResRef);
+    }
+}
---
+++ Kotor.NET/Resources/KotorIFO/IFO.cs
@@ -0,0 +1,244 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Kotor.NET.Common.Data;
+using Kotor.NET.Resources.KotorGFF;
+
+namespace Kotor.NET.Resources.KotorIFO;
+
+public class IFO
+{
+    public GFF Source { get; }
+
+    public IFO()
+    {
+        Source = new();
+    }
+    public IFO(GFF source)
+    {
+        Source = source;
+    }
+    public static IFO FromFile(string filepath)
+    {
+        return new(GFF.FromFile(filepath));
+    }
+    public static IFO FromBytes(byte[] bytes)
+    {
+        return new(GFF.FromBytes(bytes));
+    }
+    public static IFO FromStream(Stream stream)
+    {
+        return new(GFF.FromStream(stream));
+    }
+
+    /// <remarks>
+    /// This is the value stored in the <c>Mod_VO_ID</c> field in the IFO.
+    /// </remarks>
+    public string VoiceOverID
+    {
+        get => Source.Root.GetString("Mod_VO_ID") ?? "";
+        set => Source.Root.SetString("Mod_VO_ID", value);
+    }
+
+    /// <remarks>
+    /// This is the value stored in the <c>Mod_Name</c> field in the IFO.
+    /// </remarks>
+    public LocalisedString Name
+    {
+        get => Source.Root.GetLocalisedString("Mod_Name") ?? new();
+        set => Source.Root.SetLocalisedString("Mod_Name", value);
+    }
+
+    /// <remarks>
+    /// This is the value stored in the <c>Mod_Tag</c> field in the IFO.
+    /// </remarks>
+    public string Tag
+    {
+        get => Source.Root.GetString("Mod_Tag") ?? "";
+        set => Source.Root.SetString("Mod_Tag", value);
+    }
+
+    /// <remarks>
+    /// This is the value stored in the <c>Mod_Entry_Area</c> field in the IFO.
+    /// </remarks>
+    public ResRef EntryArea
+    {
+        get => Source.Root.GetResRef("Mod_Entry_Area") ?? "";
+        set => Source.Root.SetResRef("Mod_Entry_Area", value);
+    }
+
+    /// <remarks>
+    /// This is the value stored in the <c>Mod_Entry_X</c> field in the IFO.
+    /// </remarks>
+    public float ModEntryX
+    {
+        get => Source.Root.GetSingle("Mod_Entry_X") ?? 0.0f;
+        set => Source.Root.SetSingle("Mod_Entry_X", value);
+    }
+
+    /// <remarks>
+    /// This is the value stored in the <c>Mod_Entry_Y</c> field in the IFO.
+    /// </remarks>
+    public float ModEntryY
+    {
+        get => Source.Root.GetSingle("Mod_Entry_Y") ?? 0.0f;
+        set => Source.Root.SetSingle("Mod_Entry_Y", value);
+    }
+
+    /// <remarks>
+    /// This is the value stored in the <c>Mod_Entry_Z</c> field in the IFO.
+    /// </remarks>
+    public float ModEntryZ
+    {
+        get => Source.Root.GetSingle("Mod_Entry_Z") ?? 0.0f;
+        set => Source.Root.SetSingle("Mod_Entry_Z", value);
+    }
+
+    /// <remarks>
+    /// This is the value stored in the <c>Mod_Entry_Dir_X</c> field in the IFO.
+    /// </remarks>
+    public float ModEntryDirectionX
+    {
+        get => Source.Root.GetSingle("Mod_Entry_Dir_X") ?? 0.0f;
+        set => Source.Root.SetSingle("Mod_Entry_Dir_X", value);
+    }
+
+    /// <remarks>
+    /// This is the value stored in the <c>Mod_Entry_Dir_Y</c> field in the IFO.
+    /// </remarks>
+    public float ModEntryDirectionY
+    {
+        get => Source.Root.GetSingle("Mod_Entry_Dir_Y") ?? 0.0f;
+        set => Source.Root.SetSingle("Mod_Entry_Dir_Y", value);
+    }
+
+    /// <remarks>
+    /// This is the value stored in the <c>Mod_XPScale</c> field in the IFO.
+    /// </remarks>
+    public byte XPScale
+    {
+        get => Source.Root.GetUInt8("Mod_XPScale") ?? 0;
+        set => Source.Root.SetUInt8("Mod_XPScale", value);
+    }
+
+    /// <remarks>
+    /// This is the value stored in the <c>Mod_OnHeartbeat</c> field in the IFO.
+    /// </remarks>
+    public ResRef OnHeartbeat
+    {
+        get => Source.Root.GetResRef("Mod_OnHeartbeat") ?? "";
+        set => Source.Root.SetResRef("Mod_OnHeartbeat", value);
+    }
+
+    /// <remarks>
+    /// This is the value stored in the <c>Mod_OnModLoad</c> field in the IFO.
+    /// </remarks>
+    public ResRef OnModLoad
+    {
+        get => Source.Root.GetResRef("Mod_OnModLoad") ?? "";
+        set => Source.Root.SetResRef("Mod_OnModLoad", value);
+    }
+
+    /// <remarks>
+    /// This is the value stored in the <c>Mod_OnModStart</c> field in the IFO.
+    /// </remarks>
+    public ResRef OnModStart
+    {
+        get => Source.Root.GetResRef("Mod_OnModStart") ?? "";
+        set => Source.Root.SetResRef("Mod_OnModStart", value);
+    }
+
+    /// <remarks>
+    /// This is the value stored in the <c>Mod_OnClientEntr</c> field in the IFO.
+    /// </remarks>
+    public ResRef OnClientEnter
+    {
+        get => Source.Root.GetResRef("Mod_OnClientEntr") ?? "";
+        set => Source.Root.SetResRef("Mod_OnClientEntr", value);
+    }
+
+    /// <remarks>
+    /// This is the value stored in the <c>Mod_OnClientLeav</c> field in the IFO.
+    /// </remarks>
+    public ResRef OnClientLeave
+    {
+        get => Source.Root.GetResRef("Mod_OnClientLeav") ?? "";
+        set => Source.Root.SetResRef("Mod_OnClientLeav", value);
+    }
+
+    /// <remarks>
+    /// This is the value stored in the <c>Mod_OnActvtItem</c> field in the IFO.
+    /// </remarks>
+    public ResRef OnActivateItem
+    {
+        get => Source.Root.GetResRef("Mod_OnActvtItem") ?? "";
+        set => Source.Root.SetResRef("Mod_OnActvtItem", value);
+    }
+
+    /// <remarks>
+    /// This is the value stored in the <c>Mod_OnAcquirItem</c> field in the IFO.
+    /// </remarks>
+    public ResRef OnAcquireItem
+    {
+        get => Source.Root.GetResRef("Mod_OnAcquirItem") ?? "";
+        set => Source.Root.SetResRef("Mod_OnAcquirItem", value);
+    }
+
+    /// <remarks>
+    /// This is the value stored in the <c>Mod_OnUsrDefined</c> field in the IFO.
+    /// </remarks>
+    public ResRef OnUserDefined
+    {
+        get => Source.Root.GetResRef("Mod_OnUsrDefined") ?? "";
+        set => Source.Root.SetResRef("Mod_OnUsrDefined", value);
+    }
+
+    /// <remarks>
+    /// This is the value stored in the <c>Mod_OnUnAqreItem</c> field in the IFO.
+    /// </remarks>
+    public ResRef OnUnAcquireItem
+    {
+        get => Source.Root.GetResRef("Mod_OnUnAqreItem") ?? "";
+        set => Source.Root.SetResRef("Mod_OnUnAqreItem", value);
+    }
+
+    /// <remarks>
+    /// This is the value stored in the <c>Mod_OnPlrDeath</c> field in the IFO.
+    /// </remarks>
+    public ResRef OnPlayerDeath
+    {
+        get => Source.Root.GetResRef("Mod_OnPlrDeath") ?? "";
+        set => Source.Root.SetResRef("Mod_OnPlrDeath", value);
+    }
+
+    /// <remarks>
+    /// This is the value stored in the <c>Mod_OnPlrDying</c> field in the IFO.
+    /// </remarks>
+    public ResRef OnPlayerDying
+    {
+        get => Source.Root.GetResRef("Mod_OnPlrDying") ?? "";
+        set => Source.Root.SetResRef("Mod_OnPlrDying", value);
+    }
+
+    /// <remarks>
+    /// This is the value stored in the <c>Mod_OnPlrLvlUp</c> field in the IFO.
+    /// </remarks>
+    public ResRef OnPlayerLevelUp
+    {
+        get => Source.Root.GetResRef("Mod_OnPlrLvlUp") ?? "";
+        set => Source.Root.SetResRef("Mod_OnPlrLvlUp", value);
+    }
+
+    /// <remarks>
+    /// This is the value stored in the <c>Mod_OnSpawnBtnDn</c> field in the IFO.
+    /// </remarks>
+    public ResRef OnSpawnButtonDown
+    {
+        get => Source.Root.GetResRef("Mod_OnSpawnBtnDn") ?? "";
+        set => Source.Root.SetResRef("Mod_OnSpawnBtnDn", value);
+    }
+
+    public IFOAreaCollection ModAreaList => new(Source);
+}
