133764131d565165aff6e4aaa1fa03f9ae723d00
Nick Hugi <hugi.nicholas@hotmail.com>
2024-08-25 13:49:38 +0800

    Add UTP class

---
+++ Kotor.NET.Tests/Resources/KotorUTP/TestUTP.cs
@@ -0,0 +1,218 @@
+﻿using System.Linq;
+using Kotor.NET.Resources.KotorJRL;
+using Kotor.NET.Resources.KotorUTP;
+using Xunit;
+
+namespace Kotor.NET.Tests.Resources.KotorUTP;
+
+public class TestUTP
+{
+    public static readonly string File1Filepath = "Resources/KotorUTP/file1.utp";
+
+    [Fact]
+    public void Getters()
+    {
+        // Setup
+        var utp = UTP.FromFile(File1Filepath);
+
+        // Assert
+        Assert.Equal(2, utp.AnimationState);
+        Assert.Equal(67U, utp.AppearanceID);
+        Assert.True(utp.AutoRemoveKey);
+        Assert.Equal("Military High Plastic cylinder crate", utp.Comment);
+        Assert.Equal("minecntr", utp.Conversation);
+        Assert.Equal(15, utp.CurrentHP);
+        Assert.Equal(1U, utp.FactionID);
+        Assert.Equal(16, utp.Fortitude);
+        Assert.Equal(5, utp.Hardness);
+        Assert.True(utp.HasInventory);
+        Assert.Equal(15, utp.HP);
+        Assert.False(utp.IsComputer);
+        Assert.Equal("key_locker", utp.KeyName);
+        Assert.True(utp.KeyRequired);
+        Assert.False(utp.Lockable);
+        Assert.True(utp.Locked);
+        Assert.Equal(124188, utp.Name.StringRef);
+        Assert.False(utp.Min1HP);
+        Assert.True(utp.NotBlastable);
+        Assert.Equal("a_compdlg", utp.OnClosed);
+        Assert.Equal("", utp.OnDamaged);
+        Assert.Equal("", utp.OnDeath);
+        Assert.Equal("", utp.OnEndDialogue);
+        Assert.Equal("a_compdlg", utp.OnFailToOpen);
+        Assert.Equal("", utp.OnHeartbeat);
+        Assert.Equal("", utp.OnInvDisturbed);
+        Assert.Equal("", utp.OnMeleeAttacked);
+        Assert.Equal("a_set_unlocked", utp.OnOpen);
+        Assert.Equal("", utp.OnSpellCastAt);
+        Assert.Equal("", utp.OnUnlock);
+        Assert.Equal("", utp.OnUsed);
+        Assert.Equal("", utp.OnUserDefined);
+        Assert.Equal(27, utp.OpenLockDC);
+        Assert.Equal(1, utp.OpenLockDiff);
+        Assert.Equal(0, utp.OpenLockDiffMod);
+        Assert.Equal(6, utp.PaletteID);
+        Assert.True(utp.PartyInteract);
+        Assert.True(utp.Plot);
+        Assert.Equal(0, utp.Reflex);
+        Assert.False(utp.Static);
+        Assert.Equal("locker_locked", utp.Tag);
+        Assert.Equal("g_tresmilhig007", utp.ResRef);
+        Assert.Equal(0, utp.Type);
+        Assert.True(utp.Useable);
+        Assert.Equal(0, utp.Will);
+
+        Assert.Equal(3, utp.Inventory.Count);
+
+        var item2 = utp.Inventory[2];
+        Assert.Equal(item2.ResRef, "g_i_progspike01");
+    }
+
+    [Fact]
+    public void Setters()
+    {
+        // Setup
+        var utp = UTP.FromFile(File1Filepath);
+        utp.AnimationState = 1;
+        utp.AppearanceID = 2;
+        utp.AutoRemoveKey = false;
+        utp.Comment = "comment";
+        utp.Conversation = "conversation";
+        utp.CurrentHP = 3;
+        utp.FactionID = 4;
+        utp.Fortitude = 5;
+        utp.Hardness = 6;
+        utp.HasInventory = false;
+        utp.HP = 7;
+        utp.IsComputer = true;
+        utp.KeyName = "key";
+        utp.KeyRequired = false;
+        utp.Lockable = true;
+        utp.Locked = false;
+        utp.Name.StringRef = 8;
+        utp.Min1HP = true;
+        utp.NotBlastable = false;
+        utp.OnClosed = "closed";
+        utp.OnDamaged = "damaged";
+        utp.OnDeath = "death";
+        utp.OnEndDialogue = "enddialogue";
+        utp.OnFailToOpen = "failtoopen";
+        utp.OnHeartbeat = "heartbeat";
+        utp.OnInvDisturbed = "invdisturbed";
+        utp.OnMeleeAttacked = "meleeattacked";
+        utp.OnOpen = "open";
+        utp.OnSpellCastAt = "spellcastat";
+        utp.OnUnlock = "unlock";
+        utp.OnUsed = "used";
+        utp.OnUserDefined = "userdefined";
+        utp.PaletteID = 9;
+        utp.OpenLockDC = 10;
+        utp.OpenLockDiff = 11;
+        utp.OpenLockDiffMod = 12;
+        utp.PartyInteract = false;
+        utp.Plot = false;
+        utp.Reflex = 13;
+        utp.Static = true;
+        utp.Tag = "tag";
+        utp.ResRef = "resref";
+        utp.Type = 14;
+        utp.Useable = false;
+        utp.Will = 15;
+
+        // Assert
+        Assert.Equal(1, utp.AnimationState);
+        Assert.Equal(2U, utp.AppearanceID);
+        Assert.False(utp.AutoRemoveKey);
+        Assert.Equal("comment", utp.Comment);
+        Assert.Equal("conversation", utp.Conversation);
+        Assert.Equal(3, utp.CurrentHP);
+        Assert.Equal(4U, utp.FactionID);
+        Assert.Equal(5, utp.Fortitude);
+        Assert.Equal(6, utp.Hardness);
+        Assert.False(utp.HasInventory);
+        Assert.Equal(7, utp.HP);
+        Assert.True(utp.IsComputer);
+        Assert.Equal("key", utp.KeyName);
+        Assert.False(utp.KeyRequired);
+        Assert.True(utp.Lockable);
+        Assert.False(utp.Locked);
+        Assert.Equal(8, utp.Name.StringRef);
+        Assert.True(utp.Min1HP);
+        Assert.False(utp.NotBlastable);
+        Assert.Equal("closed", utp.OnClosed);
+        Assert.Equal("damaged", utp.OnDamaged);
+        Assert.Equal("death", utp.OnDeath);
+        Assert.Equal("enddialogue", utp.OnEndDialogue);
+        Assert.Equal("failtoopen", utp.OnFailToOpen);
+        Assert.Equal("heartbeat", utp.OnHeartbeat);
+        Assert.Equal("invdisturbed", utp.OnInvDisturbed);
+        Assert.Equal("meleeattacked", utp.OnMeleeAttacked);
+        Assert.Equal("open", utp.OnOpen);
+        Assert.Equal("spellcastat", utp.OnSpellCastAt);
+        Assert.Equal("unlock", utp.OnUnlock);
+        Assert.Equal("used", utp.OnUsed);
+        Assert.Equal("userdefined", utp.OnUserDefined);
+        Assert.Equal(9, utp.PaletteID);
+        Assert.Equal(10, utp.OpenLockDC);
+        Assert.Equal(11, utp.OpenLockDiff);
+        Assert.Equal(12, utp.OpenLockDiffMod);
+        Assert.False(utp.PartyInteract);
+        Assert.False(utp.Plot);
+        Assert.Equal(13, utp.Reflex);
+        Assert.True(utp.Static);
+        Assert.Equal("tag", utp.Tag);
+        Assert.Equal("resref", utp.ResRef);
+        Assert.Equal(14, utp.Type);
+        Assert.False(utp.Useable);
+        Assert.Equal(15, utp.Will);
+    }
+
+    [Fact]
+    public void AddItem()
+    {
+        // Setup
+        var utp = new UTP();
+
+        utp.Inventory.Add("item0");
+        utp.Inventory.Add("item1");
+        utp.Inventory.Add("item2");
+
+        // Assert
+        Assert.Equal(3, utp.Inventory.Count);
+        Assert.Equal(2U, utp.Source.Root.GetList("ItemList")?.ElementAt(2).ID);
+
+        var item = utp.Inventory[2];
+        Assert.True(item.Exists);
+        Assert.Equal(2, item.Index);
+        Assert.Equal("item2", item.ResRef);
+    }
+
+    [Fact]
+    public void RemoveItem()
+    {
+        // Setup
+        var utp = UTP.FromFile(File1Filepath);
+
+        //Act
+        utp.Inventory[0].Remove();
+
+        // Assert
+        Assert.Equal(2, utp.Inventory.Count);
+        Assert.Equal(0U, utp.Source.Root.GetList("ItemList")?.ElementAt(0).ID);
+        Assert.Equal(1U, utp.Source.Root.GetList("ItemList")?.ElementAt(1).ID);
+    }
+
+    [Fact]
+    public void ClearInventory()
+    {
+        // Setup
+        var utp = UTP.FromFile(File1Filepath);
+
+        // Act
+        utp.Inventory.Clear();
+
+        // Assert
+        Assert.Equal(0, utp.Inventory.Count);
+    }
+
+}
---
+++ Kotor.NET/Resources/KotorUTP/UTP.cs
@@ -0,0 +1,583 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Kotor.NET.Common.Data;
+using Kotor.NET.Resources.KotorGFF;
+
+namespace Kotor.NET.Resources.KotorUTP;
+
+public class UTP
+{
+    public GFF Source { get; }
+
+    public UTP()
+    {
+        Source = new();
+    }
+
+    public UTP(GFF source)
+    {
+        Source = source;
+    }
+
+    public static UTP FromFile(string filepath)
+    {
+        return new(GFF.FromFile(filepath));
+    }
+
+    public static UTP FromBytes(byte[] bytes)
+    {
+        return new(GFF.FromBytes(bytes));
+    }
+
+    public static UTP FromStream(Stream stream)
+    {
+        return new(GFF.FromStream(stream));
+    }
+
+    /// <summary>
+    /// 
+    /// </summary>
+    /// <remarks>
+    /// This is the value stored in the <c>AnimationState</c> field in the UTP.
+    /// </remarks>
+    public byte AnimationState
+    {
+        get => Source.Root.GetUInt8("AnimationState") ?? 0;
+        set => Source.Root.SetUInt8("AnimationState", value);
+    }
+
+    /// <summary>
+    /// 
+    /// </summary>
+    /// <remarks>
+    /// This is the value stored in the <c>Appearance</c> field in the UTP and is an index into
+    /// the <c>placeables.2da</c> file.
+    /// </remarks>
+    public uint AppearanceID
+    {
+        get => Source.Root.GetUInt32("Appearance") ?? 0;
+        set => Source.Root.SetUInt32("Appearance", value);
+    }
+
+    /// <summary>
+    /// 
+    /// </summary>
+    /// <remarks>
+    /// This is the value stored in the <c>AutoRemoveKey</c> field in the UTP.
+    /// </remarks>
+    public bool AutoRemoveKey
+    {
+        get => Source.Root.GetUInt8("AutoRemoveKey") != 0;
+        set => Source.Root.SetUInt8("AutoRemoveKey", Convert.ToByte(value));
+    }
+
+    /// <summary>
+    /// 
+    /// </summary>
+    /// <remarks>
+    /// This is the value stored in the <c>Comment</c> field in the UTP.
+    /// </remarks>
+    public string Comment
+    {
+        get => Source.Root.GetString("Comment") ?? "";
+        set => Source.Root.SetString("Comment", value);
+    }
+
+    /// <summary>
+    /// 
+    /// </summary>
+    /// <remarks>
+    /// This is the value stored in the <c>Conversation</c> field in the UTP.
+    /// </remarks>
+    public ResRef Conversation
+    {
+        get => Source.Root.GetResRef("Conversation") ?? "";
+        set => Source.Root.SetResRef("Conversation", value);
+    }
+
+    /// <summary>
+    /// 
+    /// </summary>
+    /// <remarks>
+    /// This is the value stored in the <c>CurrentHP</c> field in the UTP.
+    /// </remarks>
+    public short CurrentHP
+    {
+        get => Source.Root.GetInt16("CurrentHP") ?? 0;
+        set => Source.Root.SetInt16("CurrentHP", value);
+    }
+
+    /// <summary>
+    /// 
+    /// </summary>
+    /// <remarks>
+    /// This is the value stored in the <c>Faction</c> field in the UTP.
+    /// </remarks>
+    public uint FactionID
+    {
+        get => Source.Root.GetUInt32("Faction") ?? 0;
+        set => Source.Root.SetUInt32("Faction", value);
+    }
+
+    /// <summary>
+    /// 
+    /// </summary>
+    /// <remarks>
+    /// This is the value stored in the <c>Fort</c> field in the UTP.
+    /// </remarks>
+    public byte Fortitude
+    {
+        get => Source.Root.GetUInt8("Fort") ?? 0;
+        set => Source.Root.SetUInt8("Fort", value);
+    }
+
+    /// <summary>
+    /// 
+    /// </summary>
+    /// <remarks>
+    /// This is the value stored in the <c>Hardness</c> field in the UTP.
+    /// </remarks>
+    public byte Hardness
+    {
+        get => Source.Root.GetUInt8("Hardness") ?? 0;
+        set => Source.Root.SetUInt8("Hardness", value);
+    }
+
+    /// <summary>
+    /// 
+    /// </summary>
+    /// <remarks>
+    /// This is the value stored in the <c>HasInventory</c> field in the UTP.
+    /// </remarks>
+    public bool HasInventory
+    {
+        get => Source.Root.GetUInt8("HasInventory") != 0;
+        set => Source.Root.SetUInt8("HasInventory", Convert.ToByte(value));
+    }
+
+    /// <summary>
+    /// 
+    /// </summary>
+    /// <remarks>
+    /// This is the value stored in the <c>HP</c> field in the UTP.
+    /// </remarks>
+    public short HP
+    {
+        get => Source.Root.GetInt16("HP") ?? 0;
+        set => Source.Root.SetInt16("HP", value);
+    }
+
+    /// <summary>
+    /// 
+    /// </summary>
+    /// <remarks>
+    /// This is the value stored in the <c>IsComputer</c> field in the UTP.
+    /// </remarks>
+    public bool IsComputer
+    {
+        get => (Source.Root.GetUInt8("IsComputer") ?? 0) != 0;
+        set => Source.Root.SetUInt8("IsComputer", Convert.ToByte(value));
+    }
+
+    /// <summary>
+    /// 
+    /// </summary>
+    /// <remarks>
+    /// This is the value stored in the <c>KeyName</c> field in the UTP.
+    /// </remarks>
+    public string KeyName
+    {
+        get => Source.Root.GetString("KeyName") ?? "";
+        set => Source.Root.SetString("KeyName", value);
+    }
+
+    /// <summary>
+    /// 
+    /// </summary>
+    /// <remarks>
+    /// This is the value stored in the <c>KeyRequired</c> field in the UTP.
+    /// </remarks>
+    public bool KeyRequired
+    {
+        get => Source.Root.GetUInt8("KeyRequired") != 0;
+        set => Source.Root.SetUInt8("KeyRequired", Convert.ToByte(value));
+    }
+
+    /// <summary>
+    /// 
+    /// </summary>
+    /// <remarks>
+    /// This is the value stored in the <c>Lockable</c> field in the UTP.
+    /// </remarks>
+    public bool Lockable
+    {
+        get => Source.Root.GetUInt8("Lockable") != 0;
+        set => Source.Root.SetUInt8("Lockable", Convert.ToByte(value));
+    }
+
+    /// <summary>
+    /// 
+    /// </summary>
+    /// <remarks>
+    /// This is the value stored in the <c>Locked</c> field in the UTP.
+    /// </remarks>
+    public bool Locked
+    {
+        get => Source.Root.GetUInt8("Locked") != 0;
+        set => Source.Root.SetUInt8("Locked", Convert.ToByte(value));
+    }
+
+    /// <summary>
+    /// 
+    /// </summary>
+    /// <remarks>
+    /// This is the value stored in the <c>LocName</c> field in the UTP.
+    /// </remarks>
+    public LocalisedString Name
+    {
+        get => Source.Root.GetLocalisedString("LocName") ?? new();
+        set => Source.Root.SetLocalisedString("LocName", value);
+    }
+
+    /// <summary>
+    /// 
+    /// </summary>
+    /// <remarks>
+    /// This is the value stored in the <c>Min1HP</c> field in the UTP.
+    /// </remarks>
+    public bool Min1HP
+    {
+        get => Source.Root.GetUInt8("Min1HP") != 0;
+        set => Source.Root.SetUInt8("Min1HP", Convert.ToByte(value));
+    }
+
+    /// <summary>
+    /// 
+    /// </summary>
+    /// <remarks>
+    /// This is the value stored in the <c>NotBlastable</c> field in the UTP. Only supported by KotOR 2.
+    /// </remarks>
+    public bool NotBlastable
+    {
+        get => Source.Root.GetUInt8("NotBlastable") != 0;
+        set => Source.Root.SetUInt8("NotBlastable", Convert.ToByte(value));
+    }
+
+    /// <summary>
+    /// 
+    /// </summary>
+    /// <remarks>
+    /// This is the value stored in the <c>OnClosed</c> field in the UTP.
+    /// </remarks>
+    public ResRef OnClosed
+    {
+        get => Source.Root.GetResRef("OnClosed") ?? "";
+        set => Source.Root.SetResRef("OnClosed", value);
+    }
+
+    /// <summary>
+    /// 
+    /// </summary>
+    /// <remarks>
+    /// This is the value stored in the <c>OnDamaged</c> field in the UTP.
+    /// </remarks>
+    public ResRef OnDamaged
+    {
+        get => Source.Root.GetResRef("OnDamaged") ?? "";
+        set => Source.Root.SetResRef("OnDamaged", value);
+    }
+
+    /// <summary>
+    /// 
+    /// </summary>
+    /// <remarks>
+    /// This is the value stored in the <c>OnDeath</c> field in the UTP.
+    /// </remarks>
+    public ResRef OnDeath
+    {
+        get => Source.Root.GetResRef("OnDeath") ?? "";
+        set => Source.Root.SetResRef("OnDeath", value);
+    }
+
+    /// <summary>
+    /// 
+    /// </summary>
+    /// <remarks>
+    /// This is the value stored in the <c>OnEndDialogue</c> field in the UTP.
+    /// </remarks>
+    public ResRef OnEndDialogue
+    {
+        get => Source.Root.GetResRef("OnEndDialogue") ?? "";
+        set => Source.Root.SetResRef("OnEndDialogue", value);
+    }
+
+    /// <summary>
+    /// 
+    /// </summary>
+    /// <remarks>
+    /// This is the value stored in the <c>OnFailToOpen</c> field in the UTP. Only supported by KotOR 2.
+    /// </remarks>
+    public ResRef OnFailToOpen
+    {
+        get => Source.Root.GetResRef("OnFailToOpen") ?? "";
+        set => Source.Root.SetResRef("OnFailToOpen", value);
+    }
+
+    /// <summary>
+    /// 
+    /// </summary>
+    /// <remarks>
+    /// This is the value stored in the <c>OnHeartbeat</c> field in the UTP.
+    /// </remarks>
+    public ResRef OnHeartbeat
+    {
+        get => Source.Root.GetResRef("OnHeartbeat") ?? "";
+        set => Source.Root.SetResRef("OnHeartbeat", value);
+    }
+
+    /// <summary>
+    /// 
+    /// </summary>
+    /// <remarks>
+    /// This is the value stored in the <c>OnInvDisturbed</c> field in the UTP.
+    /// </remarks>
+    public ResRef OnInvDisturbed
+    {
+        get => Source.Root.GetResRef("OnInvDisturbed") ?? "";
+        set => Source.Root.SetResRef("OnInvDisturbed", value);
+    }
+
+    /// <summary>
+    /// 
+    /// </summary>
+    /// <remarks>
+    /// This is the value stored in the <c>OnMeleeAttacked</c> field in the UTP.
+    /// </remarks>
+    public ResRef OnMeleeAttacked
+    {
+        get => Source.Root.GetResRef("OnMeleeAttacked") ?? "";
+        set => Source.Root.SetResRef("OnMeleeAttacked", value);
+    }
+
+    /// <summary>
+    /// 
+    /// </summary>
+    /// <remarks>
+    /// This is the value stored in the <c>OnOpen</c> field in the UTP.
+    /// </remarks>
+    public ResRef OnOpen
+    {
+        get => Source.Root.GetResRef("OnOpen") ?? "";
+        set => Source.Root.SetResRef("OnOpen", value);
+    }
+
+    /// <summary>
+    /// 
+    /// </summary>
+    /// <remarks>
+    /// This is the value stored in the <c>OnSpellCastAt</c> field in the UTP.
+    /// </remarks>
+    public ResRef OnSpellCastAt
+    {
+        get => Source.Root.GetResRef("OnSpellCastAt") ?? "";
+        set => Source.Root.SetResRef("OnSpellCastAt", value);
+    }
+
+    /// <summary>
+    /// 
+    /// </summary>
+    /// <remarks>
+    /// This is the value stored in the <c>OnUnlock</c> field in the UTP.
+    /// </remarks>
+    public ResRef OnUnlock
+    {
+        get => Source.Root.GetResRef("OnUnlock") ?? "";
+        set => Source.Root.SetResRef("OnUnlock", value);
+    }
+
+    /// <summary>
+    /// 
+    /// </summary>
+    /// <remarks>
+    /// This is the value stored in the <c>OnUsed</c> field in the UTP.
+    /// </remarks>
+    public ResRef OnUsed
+    {
+        get => Source.Root.GetResRef("OnUsed") ?? "";
+        set => Source.Root.SetResRef("OnUsed", value);
+    }
+
+    /// <summary>
+    /// 
+    /// </summary>
+    /// <remarks>
+    /// This is the value stored in the <c>OnUserDefined</c> field in the UTP.
+    /// </remarks>
+    public ResRef OnUserDefined
+    {
+        get => Source.Root.GetResRef("OnUserDefined") ?? "";
+        set => Source.Root.SetResRef("OnUserDefined", value);
+    }
+
+    /// <summary>
+    /// 
+    /// </summary>
+    /// <remarks>
+    /// This is the value stored in the <c>OpenLockDC</c> field in the UTP.
+    /// </remarks>
+    public byte OpenLockDC
+    {
+        get => Source.Root.GetUInt8("OpenLockDC") ?? 0;
+        set => Source.Root.SetUInt8("OpenLockDC", value);
+    }
+
+    /// <summary>
+    /// 
+    /// </summary>
+    /// <remarks>
+    /// This is the value stored in the <c>OpenLockDiff</c> field in the UTP. Only supported by KotOR 2.
+    /// </remarks>
+    public byte OpenLockDiff
+    {
+        get => Source.Root.GetUInt8("OpenLockDiff") ?? 0;
+        set => Source.Root.SetUInt8("OpenLockDiff", value);
+    }
+
+    /// <summary>
+    /// 
+    /// </summary>
+    /// <remarks>
+    /// This is the value stored in the <c>OpenLockDiffMod</c> field in the UTP. Only supported by KotOR 2.
+    /// </remarks>
+    public sbyte OpenLockDiffMod
+    {
+        get => Source.Root.GetInt8("OpenLockDiffMod") ?? 0;
+        set => Source.Root.SetInt8("OpenLockDiffMod", value);
+    }
+
+    /// <summary>
+    /// 
+    /// </summary>
+    /// <remarks>
+    /// This is the value stored in the <c>PaletteID</c> field in the UTP.
+    /// </remarks>
+    public byte PaletteID
+    {
+        get => Source.Root.GetUInt8("PaletteID") ?? 0;
+        set => Source.Root.SetUInt8("PaletteID", value);
+    }
+
+    /// <summary>
+    /// 
+    /// </summary>
+    /// <remarks>
+    /// This is the value stored in the <c>PartyInteract</c> field in the UTP.
+    /// </remarks>
+    public bool PartyInteract
+    {
+        get => Source.Root.GetUInt8("PartyInteract") != 0;
+        set => Source.Root.SetUInt8("PartyInteract", Convert.ToByte(value));
+    }
+
+    /// <summary>
+    /// 
+    /// </summary>
+    /// <remarks>
+    /// This is the value stored in the <c>Plot</c> field in the UTP.
+    /// </remarks>
+    public bool Plot
+    {
+        get => Source.Root.GetUInt8("Plot") != 0;
+        set => Source.Root.SetUInt8("Plot", Convert.ToByte(value));
+    }
+
+    /// <summary>
+    /// 
+    /// </summary>
+    /// <remarks>
+    /// This is the value stored in the <c>Ref</c> field in the UTP.
+    /// </remarks>
+    public byte Reflex
+    {
+        get => Source.Root.GetUInt8("Ref") ?? 0;
+        set => Source.Root.SetUInt8("Ref", value);
+    }
+
+    /// <summary>
+    /// 
+    /// </summary>
+    /// <remarks>
+    /// This is the value stored in the <c>Static</c> field in the UTP.
+    /// </remarks>
+    public bool Static
+    {
+        get => Source.Root.GetUInt8("Static") != 0;
+        set => Source.Root.SetUInt8("Static", Convert.ToByte(value));
+    }
+
+    /// <summary>
+    /// 
+    /// </summary>
+    /// <remarks>
+    /// This is the value stored in the <c>Tag</c> field in the UTP.
+    /// </remarks>
+    public string Tag
+    {
+        get => Source.Root.GetString("Tag") ?? "";
+        set => Source.Root.SetString("Tag", value);
+    }
+
+    /// <summary>
+    /// 
+    /// </summary>
+    /// <remarks>
+    /// This is the value stored in the <c>TemplateResRef</c> field in the UTP.
+    /// </remarks>
+    public ResRef ResRef
+    {
+        get => Source.Root.GetResRef("TemplateResRef") ?? "";
+        set => Source.Root.SetResRef("TemplateResRef", value);
+    }
+
+    /// <summary>
+    /// 
+    /// </summary>
+    /// <remarks>
+    /// This is the value stored in the <c>Type</c> field in the UTP.
+    /// </remarks>
+    public byte Type
+    {
+        get => Source.Root.GetUInt8("Type") ?? 0;
+        set => Source.Root.SetUInt8("Type", value);
+    }
+
+    /// <summary>
+    /// 
+    /// </summary>
+    /// <remarks>
+    /// This is the value stored in the <c>Useable</c> field in the UTP.
+    /// </remarks>
+    public bool Useable
+    {
+        get => Source.Root.GetUInt8("Useable") != 0;
+        set => Source.Root.SetUInt8("Useable", Convert.ToByte(value));
+    }
+
+    /// <summary>
+    /// 
+    /// </summary>
+    /// <remarks>
+    /// This is the value stored in the <c>Will</c> field in the UTP.
+    /// </remarks>
+    public byte Will
+    {
+        get => Source.Root.GetUInt8("Will") ?? 0;
+        set => Source.Root.SetUInt8("Will", value);
+    }
+
+    public UTPItemCollection Inventory => new UTPItemCollection(Source);
+}
+
