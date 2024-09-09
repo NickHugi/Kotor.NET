01989ab8b08f2c9fe9d0219900c9e2f5b1665f2b
Nick Hugi <hugi.nicholas@hotmail.com>
2024-09-09 21:31:07 +0800

    Add RIM class

---
+++ Kotor.NET/Formats/BinaryRIM/RIMBinarySerializer.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Kotor.NET.Resources.KotorRIM;
+
+namespace Kotor.NET.Formats.BinaryRIM;
+
+public class RIMBinarySerializer
+{
+    private RIM _rim { get; }
+
+    public RIMBinarySerializer(RIM rim)
+    {
+        _rim = rim;
+    }
+
+    public RIMBinary Serialize()
+    {
+        var binary = new RIMBinary();
+
+        binary.FileHeader.FileType = RIMBinaryFileHeader.FILE_TYPES[0];
+        binary.FileHeader.FileVersion = RIMBinaryFileHeader.FILE_VERSION;
+
+        foreach (var resource in _rim)
+        {
+            binary.ResourceEntries.Add(new()
+            {
+                ResRef = resource.ResRef,
+                ResourceID = resource.Index,
+                ResourceTypeID = resource.Type.ID,
+            });
+
+            binary.ResourceData.Add(resource.Data);
+        }
+
+        binary.Recalculate();
+
+        return binary;
+    }
+}
---
+++ Kotor.NET/Resources/KotorRIM/RIM.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Kotor.NET.Common.Data;
+using Kotor.NET.Formats.BinaryRIM;
+
+namespace Kotor.NET.Resources.KotorRIM;
+
+public class RIM : IEnumerable<RIMResource>
+{
+    internal List<RIMResource> _resources;
+
+    public RIM()
+    {
+        _resources = new();
+    }
+    public static RIM FromFile(string filepath)
+    {
+        using var stream = File.OpenRead(filepath);
+        return FromStream(stream);
+    }
+    public static RIM FromBytes(byte[] bytes)
+    {
+        using var stream = new MemoryStream(bytes);
+        return FromStream(stream);
+    }
+    public static RIM FromStream(Stream stream)
+    {
+        var binary = new RIMBinary(stream);
+        var deserializer = new RIMBinaryDeserializer(binary);
+        return deserializer.Deserialize();
+    }
+
+    public IEnumerator<RIMResource> GetEnumerator() => _resources.GetEnumerator();
+    IEnumerator IEnumerable.GetEnumerator() => _resources.GetEnumerator();
+
+    public RIMResource Add(ResRef resref, ResourceType type, byte[] data)
+    {
+        var resource = new RIMResource(this, resref, type, data);
+        _resources.Add(resource);
+        return resource;
+    }
+}
