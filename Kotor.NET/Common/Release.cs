b76f94f3fe7d6b2b54780e5a1bd107f96c31eea9
NickHugi <hugi.nicholas@hotmail.com>
2025-03-17 21:11:55 +0800

    Add class for locating existing installations of KotOR on the current device

---
+++ Kotor.NET/Common/Release.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Kotor.NET.Common;
+
+public enum Release
+{
+    Disc,
+    Steam,
+    GOG,
+    Amazon,
+    GooglePlay,
+    AppleAppStore,
+}
