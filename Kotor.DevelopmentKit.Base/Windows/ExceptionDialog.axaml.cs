3ec3406bb479207b1b5ff81cce51733f0d51bbfb
Nick Hugi <hugi.nicholas@hotmail.com>
2025-03-18 22:34:00 +0800

    Exception dialog

---
+++ Kotor.DevelopmentKit.Base/Windows/ExceptionDialog.axaml.cs
@@ -0,0 +1,13 @@
+using Avalonia;
+using Avalonia.Controls;
+using Avalonia.Markup.Xaml;
+
+namespace Kotor.DevelopmentKit.Base;
+
+public partial class ExceptionDialog : Window
+{
+    public ExceptionDialog()
+    {
+        InitializeComponent();
+    }
+}
