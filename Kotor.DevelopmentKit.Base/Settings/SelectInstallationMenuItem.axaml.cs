dead4811a40dccffa89b5f4bf697418129752bd5
Nick Hugi <hugi.nicholas@hotmail.com>
2025-06-23 21:22:10 +0800

    installation list loading and selecting

--- Kotor.DevelopmentKit.Base/Settings/SelectInstallationMenuItem.axaml.cs
+++ Kotor.DevelopmentKit.Base/Settings/SelectInstallationMenuItem.axaml.cs
@@ -1,22 +1,28 @@
 using System;
 using System.Collections.ObjectModel;
 using System.Linq;
+using System.Reactive;
+using System.Reactive.Linq;
+using System.Reflection;
 using Avalonia;
 using Avalonia.Controls;
+using Avalonia.Data;
 using Avalonia.Markup.Xaml;
+using DynamicData.Binding;
+using Microsoft.Extensions.DependencyInjection;
 using ReactiveUI;
 
 namespace Kotor.DevelopmentKit.Base.Settings;
 
 public partial class SelectInstallationMenuItem : MenuItem
 {
-    public static readonly StyledProperty<Installation> SelectedInstallationProperty
-        = AvaloniaProperty.Register<SelectInstallationMenuItem, Installation>(nameof(SelectedInstallation));
+    public static readonly StyledProperty<Installation?> SelectedInstallationProperty
+        = AvaloniaProperty.Register<SelectInstallationMenuItem, Installation?>(nameof(SelectedInstallation), defaultBindingMode: BindingMode.TwoWay);
 
     public static readonly StyledProperty<ObservableCollection<Installation>> InstallationsProperty
         = AvaloniaProperty.Register<SelectInstallationMenuItem, ObservableCollection<Installation>>(nameof(Installations));
 
-    public Installation SelectedInstallation
+    public Installation? SelectedInstallation
     {
         get => GetValue(SelectedInstallationProperty);
         set => SetValue(SelectedInstallationProperty, value);
@@ -27,23 +33,44 @@ public partial class SelectInstallationMenuItem : MenuItem
         get => GetValue(InstallationsProperty);
     }
 
-    protected override Type StyleKeyOverride => typeof(MenuItem);
-
-    public ObservableCollection<Control> Itemz =>
+    public ObservableCollection<Control> MenuItems =>
     [
-        new MenuItem() { Header = "None" },
+        new MenuItem()
+        {
+            Header = "None",
+            [!MenuItem.IsCheckedProperty] = new Binding() { Source = this, Path = nameof(SelectedInstallation) },
+            ToggleType = MenuItemToggleType.Radio,
+            Command = ReactiveCommand.Create(() => SelectInstallation(null)),
+        },
         new Separator(),
-        ..Installations?.Select(x => new MenuItem() { Header = x.Name }) ?? []
+        ..Installations.Select(x => new MenuItem()
+            {
+                [!MenuItem.HeaderProperty] = new Binding() { Source = x, Path = nameof(x.Name) },
+                [!MenuItem.IsCheckedProperty] = new Binding() { Source = this, Path = nameof(SelectedInstallation) },
+                ToggleType = MenuItemToggleType.Radio,
+                Command = ReactiveCommand.Create(() => SelectInstallation(x)),
+            })
     ];
+
+    protected override Type StyleKeyOverride => typeof(MenuItem);
 
 
     public SelectInstallationMenuItem()
     {
         InitializeComponent();
 
-        this.WhenAnyValue(x => x.Installations).Subscribe(x =>
-        {
-            MenuItemX.ItemsSource = Itemz;
-        });
+        this.WhenAnyValue(x => x.Installations)
+            .WhereNotNull()
+            .Select(x => x.ToObservableChangeSet())
+            .Switch()
+            .Subscribe(x =>
+            {
+                MenuItem.ItemsSource = MenuItems;
+            });
+    }
+
+    public void SelectInstallation(Installation? installation)
+    {
+        SelectedInstallation = installation;
     }
 }
