998a5ac222eec0ca56b2c399de309c3f8dfe124b
Nick <hugi.nicholas@hotmail.com>
2026-03-28 12:45:42 +0800

    wip kit editor

---
+++ Kotor.DevelopmentKit.AreaDesigner/Views/KitEditorWindow.axaml.cs
@@ -0,0 +1,13 @@
+using Avalonia;
+using Avalonia.Controls;
+using Avalonia.Markup.Xaml;
+
+namespace Kotor.DevelopmentKit.AreaDesigner;
+
+public partial class KitEditorWindow : Window
+{
+    public KitEditorWindow()
+    {
+        InitializeComponent();
+    }
+}
