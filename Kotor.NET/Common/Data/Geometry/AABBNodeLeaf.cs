7c7ce7b0b6daac8275af0f9fe2b4c228eada327f
Nick Hugi <hugi.nicholas@hotmail.com>
2026-05-09 11:23:03 +0800

    split aabbnode classes into different files

---
+++ Kotor.NET/Common/Data/Geometry/AABBNodeLeaf.cs
@@ -0,0 +1,16 @@
+﻿namespace Kotor.NET.Common.Data.Geometry;
+
+public class AABBNodeLeaf : AABBNode
+{
+    public Face Face { get; init; }
+
+    public AABBNodeLeaf(BoundingBox boundingBox, Face face) : base(boundingBox)
+    {
+        Face = face;
+    }
+
+    public override IEnumerable<AABBNode> GetDescendants()
+    {
+        return [];
+    }
+}
