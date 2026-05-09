03e96c78ac44c1891929e253047c8e817a9c19e5
Nick Hugi <hugi.nicholas@hotmail.com>
2026-05-09 16:20:38 +0800

    wip

---
+++ Kotor.DevelopmentKit.AreaDesigner/relocate/DesignerResourceManager.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Linq;
+using Kotor.NET.Resources.KotorTPC;
+using Kotor.NET.Resources.KotorTPC.TextureFormats;
+using Kotor.NET.Tools;
+
+namespace Kotor.DevelopmentKit.AreaDesigner.relocate;
+
+public class DesignerResourceManager
+{
+    public string KitDirectory { get; set; }
+
+    private List<string> _files;
+
+    public DesignerResourceManager(string kitDirectory)
+    {
+        KitDirectory = kitDirectory;
+        _files = Directory.GetFiles(kitDirectory, "*.*", SearchOption.AllDirectories).ToList();
+    }
+
+    public TPC GetTexture(string texture)
+    {
+        var file = _files.FirstOrDefault(x =>
+            Path.GetExtension(x).Equals(".tpc", StringComparison.OrdinalIgnoreCase)
+            && Path.GetFileName(x).Equals(texture, StringComparison.OrdinalIgnoreCase));
+
+        if (file is null)
+        {
+            return new TPC(64, 64, 1, 1, TPCTextureFormat.RGB);
+        }
+        else
+        {
+            return TPC.FromFile(file);
+        }
+    }
+}
---
+++ Kotor.NET/Tools/IResourceManager.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Kotor.NET.Common;
+using Kotor.NET.Common.Data;
+using Kotor.NET.Resources.KotorMDL;
+using Kotor.NET.Resources.KotorTLK;
+using Kotor.NET.Resources.KotorTPC;
+using Kotor.NET.Resources.KotorUTC;
+using Kotor.NET.Resources.KotorUTD;
+
+namespace Kotor.NET.Tools;
+
+public interface IResourceManager
+{
+    public TPC GetTexture(ResRef resref);
+}
