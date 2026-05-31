c9826789d23ee0b9ea40f6a1a1b3b880b3046789
Nick <hugi.nicholas@hotmail.com>
2026-05-31 23:47:15 +0800

    AddObjectMode

--- Kotor.DevelopmentKit.AreaDesigner/relocate/Mode/AddObjectMode.cs
+++ Kotor.DevelopmentKit.AreaDesigner/relocate/Mode/AddObjectMode.cs
@@ -8,12 +8,26 @@ using Kotor.NET.Graphics;
 using Kotor.NET.Graphics.Cameras;
 using Kotor.NET.Graphics.Model;
 using Kotor.NET.Graphics.OpenGL;
+using ReactiveUI;
 
 namespace Kotor.DevelopmentKit.AreaDesigner.relocate.Mode;
 
 public class AddObjectMode : BaseMode
 {
     public override string Name => "Add Object";
+
+    public List<ObjectTemplate> ObjectTemplates
+    {
+        get
+        {
+            return SelectedKit?.Objects.ToList() ?? [];
+        }
+    }
+    public ObjectTemplate? SelectedObjectTemplate
+    {
+        get => field;
+        set => this.RaiseAndSetIfChanged(ref field, value);
+    }
 
     private Object _addObject = null;
     private float angle = 0;
@@ -24,12 +38,15 @@ public class AddObjectMode : BaseMode
 
     public override async Task RenderIntercept(OrbitCamera camera, Point mouse, List<MeshDescriptor> descriptors)
     {
+        if (SelectedObjectTemplate is null)
+            return;
+
         var ray = camera.ProjectRay((int)mouse.X, (int)mouse.Y, _engine.Width, _engine.Height);
         var point = ray.FindPointOnPlane(Axis.Z, 0);
 
         // todo - should be placed in room where walkmesh intersects.
         // todo - should not be hardcoded
-        _addObject = new(_area.Rooms.Last(), Kit.Manager.Get("sandral").Object("sandral_object_0")); 
+        _addObject = new(_area.Rooms.Last(), SelectedObjectTemplate); 
         _addObject.LocalPosition = point;
         _addObject.LocalOrientation = Quaternion.CreateFromYawPitchRoll(0, 0, angle * (float)Math.PI / 180);
 
@@ -41,6 +58,9 @@ public class AddObjectMode : BaseMode
 
     public override async Task Trigger()
     {
+        if (SelectedObjectTemplate is null)
+            return;
+
         // todo - add to room within bounds of the cursor
         var room = _area.Rooms.First();
         room.AddObject(_addObject);
